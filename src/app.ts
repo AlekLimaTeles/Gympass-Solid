12ff239f4cedcea9b1414d09ce5baf753a7ebe57
AlekLimaTeles <152651216+AlekLimaTeles@users.noreply.github.com>
2024-01-17 09:24:41 -0300

    Integração com Prisma ORM - Fundamentos do Prisma ORM
    Command Lines :
    "npx prisma generate",
    "npm i prisma -D",
    "npm i @prisma/client"

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
 import fastify from "fastify";
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+    data: {
+        name: 'Diego Fernandes',
+        email: 'diegofernandes@gmail.com',
+    },
+})
+
+// ORM - Object Relational Mapper
+
