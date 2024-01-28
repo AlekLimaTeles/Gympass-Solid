18e12f77fe77fde5e9e6bbbe04a5a7efac34c8ba
AlekLimaTeles <152651216+AlekLimaTeles@users.noreply.github.com>
2024-01-28 04:05:56 -0300

    Implementando casos de uso - Utilizando Factory Pattern

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,8 +1,7 @@
 import { z } from 'zod'
 import { FastifyRequest, FastifyReply } from 'fastify'
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { AuthenticateUseCase } from '@/use-cases/authenticate'
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 
 export  async function authenticate (request: FastifyRequest, reply: FastifyReply) {
     const authenticateBodySchema = z.object({
@@ -13,11 +12,10 @@ export  async function authenticate (request: FastifyRequest, reply: FastifyRepl
     const {  email, password } = authenticateBodySchema.parse(request.body)
 
     try {
-        const usersRepository = new PrismaUsersRepository()
-        const authenticateUserCase = new AuthenticateUseCase(usersRepository)
+        const authenticateUseCase = makeAuthenticateUseCase()
 
 
-        await authenticateUserCase.execute({
+        await authenticateUseCase.execute({
             email,
             password,
         })
