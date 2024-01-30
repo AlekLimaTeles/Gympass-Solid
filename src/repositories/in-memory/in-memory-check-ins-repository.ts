fe58311e64505bff60a6ae3e11b264714e7d4d12
AlekLimaTeles <152651216+AlekLimaTeles@users.noreply.github.com>
2024-01-30 17:30:49 -0300

    Implementando casos de uso - Caso de uso de check-in

---
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -0,0 +1,28 @@
+import { Prisma, CheckIn } from '@prisma/client'
+import { randomUUID } from 'crypto'
+import { CheckInsRepository } from '../check-ins-repository'
+
+export class inMemoryCheckInsRepository implements CheckInsRepository {
+    public items: CheckIn[] = []
+
+    
+    async create(data: Prisma.CheckInUncheckedCreateInput) {
+      const checkIn = {
+            id: randomUUID(),
+            user_id: data.user_id,
+            gym_id: data.gym_id,
+            validated_at: data.validated_at ? new Date(data.validated_at): null,
+            created_at: new Date(),
+        }
+
+        this.items.push(checkIn)
+
+        return checkIn
+    }
+}
+
+// async findByEmail(email) {
+//     return null
+// },
+
+// async create(data) {
