aa6b7ac6b7263da423ae72e2b3884c2800705f1a
AlekLimaTeles <152651216+AlekLimaTeles@users.noreply.github.com>
2024-02-14 06:44:09 -0300

    Controllers & Testes E2E - Teste E2E da autenticação

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,5 +1,33 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { describe, it, expect, beforeAll,afterAll } from 'vitest'
 
-test('ok', () => {
 
-}) 
+describe('Authenticate (e2e)', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('should be able to authenticate', async () => {
+        await request(app.server).post('/users')
+        .send({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '1234567',
+        })
+
+        const response = await request(app.server).post('/session')
+        .send({
+            email: 'johndoe@example.com',
+            password: '1234567',
+        })
+
+        expect(response.statusCode).toEqual(200)
+        expect(response.body).toEqual({
+            token: expect.any(String),
+        })
+    })
+})
