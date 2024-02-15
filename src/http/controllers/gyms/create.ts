fedcde084fa327b5c1021ce4e29e038349685158
AlekLimaTeles <152651216+AlekLimaTeles@users.noreply.github.com>
2024-02-15 11:32:58 -0300

    Controllers & Testes E2E - Outros controllers da academia

--- src/http/controllers/gyms/create.ts
+++ src/http/controllers/gyms/create.ts
@@ -2,16 +2,16 @@ import { z } from 'zod'
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { makeCreateGymUseCase } from '@/use-cases/factories/make-create-gym-use-case'
 
-export  async function register (request: FastifyRequest, reply: FastifyReply) {
+export  async function create (request: FastifyRequest, reply: FastifyReply) {
     const createGymBodySchema = z.object({
         title: z.string(),
         description: z.string().nullable(),
         phone: z.string().nullable(),
         latitude: z.number().refine((value) => {
-            return Math.abs(value) =< 90
+            return Math.abs(value) <= 90
         }),
         longitude: z.number().refine((value) => {
-            return Math.abs(value) =< 180
+            return Math.abs(value) <= 180
         }),
     })
 
